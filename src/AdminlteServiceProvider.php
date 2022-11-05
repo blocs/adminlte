1e2c16f3e3184e84b20b4066b598172f732a1a72
Hiroyuki Yada <yada@linear-jp.biz>
2022-11-05 22:39:49 +0900

    デプロイツールを共通化

--- src/AdminlteServiceProvider.php
+++ src/AdminlteServiceProvider.php
@@ -2,24 +2,23 @@
 
 namespace Blocs;
 
-use App\Console\Commands\BlocsAdminlte;
 use Illuminate\Support\ServiceProvider;
 
 class AdminlteServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        $this->registerBlocsAdminlteCommand();
+        $this->registerBlocsCommand();
     }
 
     public function boot()
     {
     }
 
-    public function registerBlocsAdminlteCommand()
+    public function registerBlocsCommand()
     {
         $this->app->singleton('command.blocs.adminlte', function ($app) {
-            return new BlocsAdminlte();
+            return new \App\Console\Commands\Blocs('blocs:adminlte', 'Deploy blocs/adminlte package');
         });
 
         $this->commands('command.blocs.adminlte');
